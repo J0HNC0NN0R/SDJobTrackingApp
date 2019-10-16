6ad2d6d5a795efacbea8dac4e9dc73af6b7d3a00
Daniel Bautista <danielbautista@Administrators-MacBook-Pro.local>
2019-10-16 14:07:38 -0600

    Update - Entity/test

--- JPASDJobTrackingApplication/src/main/java/com/skilldistillery/jobtracking/entities/User.java
+++ JPASDJobTrackingApplication/src/main/java/com/skilldistillery/jobtracking/entities/User.java
@@ -1,17 +1,30 @@
 package com.skilldistillery.jobtracking.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name="email")
 	private String username;
 	//finish user entity when table is defined.
 	
 	// C O N S T R U C T O R S
+	
+	public User() {}
 
 	public User(int id, String username) {
 		super();
 		this.id = id;
 		this.username = username;
 	}
+
 
 	public int getId() {
 		return id;

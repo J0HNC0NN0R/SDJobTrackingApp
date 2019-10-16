acc05cfe969c9053f31ccfd18ea23760892ce3d5
Connor Campora <connorcampora@Brucebs-MacBook-Pro.local>
2019-10-16 13:36:46 -0600

    stubbed out boot project

---
+++ SDJobTracker/src/main/java/com/skilldistillery/jobtracking/SdJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jobtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdJobTrackerApplication.class, args);
+	}
+
+}

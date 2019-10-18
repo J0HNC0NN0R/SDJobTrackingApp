2dee01c232ac819cabadd052be06b111b823cd05
Casey Asher <casey.e.asher@outlook.com>
2019-10-18 14:02:17 -0600

    all forseable components created

--- ngJobTracker/src/app/app.module.ts
+++ ngJobTracker/src/app/app.module.ts
@@ -11,6 +11,16 @@ import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './components/header/header.component';
+import { StudentProfileComponent } from './components/student-profile/student-profile.component';
+import { AdminProfileComponent } from './components/admin-profile/admin-profile.component';
+import { ApplicationComponent } from './components/application/application.component';
+import { HardmodeCountdownComponent } from './components/hardmode-countdown/hardmode-countdown.component';
+import { CohortCreateComponent } from './components/cohort-create/cohort-create.component';
+import { CohortProfileComponent } from './components/cohort-profile/cohort-profile.component';
+import { MessagingComponent } from './components/messaging/messaging.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { ApplicationStatusComponent } from './components/application-status/application-status.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +30,17 @@ import { HeaderComponent } from './components/header/header.component';
     LoginComponent,
     LogoutComponent,
     RegisterComponent,
-    HeaderComponent
+    HeaderComponent,
+    StudentProfileComponent,
+    AdminProfileComponent,
+    ApplicationComponent,
+    HardmodeCountdownComponent,
+    CohortCreateComponent,
+    CohortProfileComponent,
+    MessagingComponent,
+    StatisticsComponent,
+    ApplicationStatusComponent
+
   ],
   imports: [
     BrowserModule,

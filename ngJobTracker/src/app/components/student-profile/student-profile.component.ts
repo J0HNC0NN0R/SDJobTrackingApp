882c7a511e12babb9384f4e863ef8e69c9a2176a
BrandenBent <bent_branden@hotmail.com>
2019-10-21 11:00:21 -0600

    student-profile final changes...

--- ngJobTracker/src/app/components/student-profile/student-profile.component.ts
+++ ngJobTracker/src/app/components/student-profile/student-profile.component.ts
@@ -1,56 +1,62 @@
 import { AuthService } from 'src/app/services/auth.service';
 import { Student } from './../../models/student';
-
+​
 import { StudentService } from './../../services/student.service';
 import { Component, OnInit } from '@angular/core';
-
+​
 @Component({
   selector: 'app-student-profile',
   templateUrl: './student-profile.component.html',
   styleUrls: ['./student-profile.component.css']
 })
 export class StudentProfileComponent implements OnInit {
-
+​
   studentProfile: Student = null;
   editStudent: Student = null;
-  student: Student = null;
-
+  upStudent: Student = null;
+​
   constructor(private studentService: StudentService, private authService: AuthService) { }
-
+​
   ngOnInit() {
-    this.showProfile();
-    // this.student = this.studentProfile;
-
-
-
+    this.reload();
+    this.editStudent = this.studentProfile;
   }
-
-  updateStudent(editStudent) {
-    this.studentService.update(editStudent).subscribe(
+​
+  updateStudent() {
+    console.log('edit student' + this.upStudent);
+​
+    this.studentService.update(this.upStudent).subscribe(
       data => {
         this.showProfile();
-        console.log('TeST LOG in update Student');
-
       },
       err => {
         console.error('Error updating student' + err);
-
+​
       }
     );
   }
-
+​
+  reload() {
+    this.showProfile();
+  }
+setEditStudent(){
+this.upStudent = Object.assign({}, this .editStudent);
+}
+cancelEditStudent(){
+  this.upStudent = null;
+}
   showProfile() {
     this.studentService.getStudentByUsername().subscribe(
       data => {
-        this.studentProfile = data;
-
+        this.editStudent = data;
+​
       },
       err => {
         console.error('Error in getStudent ' + err);
       }
-
+​
     );
-
+​
   }
-
+​
 }

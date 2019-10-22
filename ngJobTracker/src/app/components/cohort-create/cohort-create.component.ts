a2c43e212aac98f20019ac468746711a9fcaac85
Casey Asher <casey.e.asher@outlook.com>
2019-10-22 09:36:00 -0600

    create cohort working. Edit cohort not working

--- ngJobTracker/src/app/components/cohort-create/cohort-create.component.ts
+++ ngJobTracker/src/app/components/cohort-create/cohort-create.component.ts
@@ -1,4 +1,8 @@
+import { CohortComponent } from './../cohort/cohort.component';
+import { StudentService } from './../../services/student.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Cohort } from 'src/app/models/cohort';
 
 @Component({
   selector: 'app-cohort-create',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cohort-create.component.css']
 })
 export class CohortCreateComponent implements OnInit {
-
-  constructor() { }
+  showAddForm: boolean;
+  showEditForm: boolean;
+  cohorts: Cohort[] = [];
+  cohort: Cohort;
+  constructor(private service: StudentService) {}
 
   ngOnInit() {
+    this.populateCohorts();
   }
 
+  createCohort(form: NgForm) {
+    console.log(form.value);
+    this.service.addCohort(form.value).subscribe(
+      data => {
+        console.log('Cohort Create Component createCohort() cohort created ');
+      },
+      err => {
+        console.error(
+          'Cohort Create Component createCohort() cohort create failed'
+        );
+        console.error(err);
+      }
+    );
+  }
+
+  populateCohorts() {
+    this.service.cohortsIndex().subscribe(
+      lifeIsGood => {
+        // console.log('Cohorts Loaded');
+        this.cohorts = lifeIsGood;
+        console.log(typeof(this.cohorts));
+        console.log(this.cohorts);
+      },
+      whenThingsGoBad => {
+        console.error('cohort create populateCohorts() Error');
+      }
+    );
+  }
 }

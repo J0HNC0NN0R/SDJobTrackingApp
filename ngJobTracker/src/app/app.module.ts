450e21bc1a70bc411c274044bb3be44a2e092e25
BrandenBent <bent_branden@hotmail.com>
2019-10-23 09:37:03 -0600

    admin statistics stub

--- ngJobTracker/src/app/app.module.ts
+++ ngJobTracker/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CohortComponent } from './components/cohort/cohort.component';
 import { JobListingComponent } from './components/job-listing/job-listing.component';
+import { AdminStatisticsComponent } from './components/admin-statistics/admin-statistics.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { JobListingComponent } from './components/job-listing/job-listing.compon
     ApplicationStatusComponent,
     FormModalComponent,
     CohortComponent,
-    JobListingComponent
+    JobListingComponent,
+    AdminStatisticsComponent
 
   ],
   imports: [
